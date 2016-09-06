71255f968ca20dff0c8db9204658f655a09d18cb
Toni Nu Tran <toni.n.tran@gmail.com>
2016-09-06 00:07:42 -0700

    Updated HTML and JS.

---
+++ script.js
@@ -0,0 +1,6 @@
+$.getJSON('http://dvc-raspberrypi.ucsd.edu:5000/door',
+    function(data) {
+        var value = data.isOpen == true ? "open." : "closed."
+        $('#doorState').text(value);
+    }
+);
