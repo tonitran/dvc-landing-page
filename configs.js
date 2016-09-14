b29b93ed1eee1c6bef3494d21ce4780ecc8ac060
Toni Tran <toni.n.tran@gmail.com>
2016-09-14 13:00:03 -0700

    Reorganized structure and added env var config.js.

---
+++ configs.js
@@ -0,0 +1,5 @@
+var prod = 'http://dvc-raspberrypi.ucsd.edu:5000';
+var local = 'http://dvc-raspberrypi.local.com:5000';
+
+// Set this to change endpoints.
+var origin = local;
--- script.js
+++
@@ -1,7 +0,0 @@
-// Production
-$.getJSON('http://dvc-raspberrypi.ucsd.edu:5000/door',
-    function(data) {
-        var value = data.isOpen == true ? "open." : "closed."
-        $('#doorState').text(value);
-    }
-);
